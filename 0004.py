8c3014e84df187fd1c98a7339ab2b2ba2f735973
ahmedelsayed08-sketch <ahmedd.elsayedd08@gmail.com>
2025-09-27 00:11:41 +0300

    Add files via upload

---
+++ 0004.py
@@ -0,0 +1,17 @@
+num=10
+if num > 0:
+    print('pos')
+elif num == 0:
+    print('the num eq 0')
+else :
+    print('neg')
+
+
+for i in range(10):
+    print(i)
+
+
+l=0
+while l<10:
+    print(l)
+    l += 1 
